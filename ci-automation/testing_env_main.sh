3dcacbc6fbd9f51533f324958d9014bd2ceeed98
Liora Milbaum <lmilbaum@redhat.com>
2018-11-27 16:58:19 +0200

    .

--- ci-automation/testing_env_main.sh
+++ ci-automation/testing_env_main.sh
@@ -15,18 +15,14 @@ on_exit() {
     exit
 }
 
-build_images() {
-  oc create -f ci-automation/config/s2i/libvirtd/libvirtd-buildconfig-template.yaml -n ember-csi
-  oc create -f ci-automation/config/s2i/ember-csi/ember-csi-buildconfig-template.yaml -n ember-csi
-}
-
 up() {
     trap 'on_exit' SIGTERM SIGINT SIGHUP EXIT
-  	build_images
-  	docker run -d --privileged -d -e 'container=docker' \
-  		-v $(pwd)/../:/root/ember-csi \
-  		--name centos-test-env centos-test-env
-  	docker exec -i centos-test-env /root/ember-csi/ci-automation/testing_env_up.sh
+    oc new-app https://github.com/lioramilbaum/ember-csi.git --context-dir=ci-automation
+
+  	# docker run -d --privileged -d -e 'container=docker' \
+  	# 	-v $(pwd)/../:/root/ember-csi \
+  	# 	--name centos-test-env centos-test-env
+  	# docker exec -i centos-test-env /root/ember-csi/ci-automation/testing_env_up.sh
 }
 
 if [[ "$command" = "up" ]]; then
